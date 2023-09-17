3bdce48bc1ffe47de5b06d74a3b08b332c2eee60
IamDhruvDarji <20231244@mywhitecliffe.com>
2023-09-18 10:18:16 +1200

    First day of coding

---
+++ Learning_Process/Hello.py
@@ -0,0 +1,24 @@
+print("Hello, World")
+print("I am Dhruv")
+# exit()
+
+# use of if
+if 5 > 2:
+    print("Five is greater than two!")
+
+# veriables
+x = ("hello, world!")
+y = 10
+
+print(x, y)
+
+First_number = 15
+Second_number = 20
+
+print("Number 1 is ", First_number)
+print("Number 2 is ", Second_number)
+print("the sum is = ", int(First_number)+int(Second_number))
+
+# input command
+Choice = input("Enter your favourite number:  ")
+print("Your favourite number is ", Choice)
